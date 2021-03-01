4b938af066a9e59f7b249c954c346ec725845a98
TheTrueRoy <66959950+TheTrueRoy@users.noreply.github.com>
2021-03-01 00:26:53 -0500

    Create s5.cpp

---
+++ CCC/2014/s5.cpp
@@ -0,0 +1,13 @@
+﻿'CCC.exe' (Win32): Loaded 'C:\Users\robsc\source\repos\CCC\x64\Debug\CCC.exe'. Symbols loaded.
+'CCC.exe' (Win32): Loaded 'C:\Windows\System32\ntdll.dll'. 
+'CCC.exe' (Win32): Loaded 'C:\Windows\System32\kernel32.dll'. 
+'CCC.exe' (Win32): Loaded 'C:\Windows\System32\KernelBase.dll'. 
+'CCC.exe' (Win32): Loaded 'C:\Windows\System32\msvcp140d.dll'. 
+'CCC.exe' (Win32): Loaded 'C:\Windows\System32\vcruntime140d.dll'. 
+'CCC.exe' (Win32): Loaded 'C:\Windows\System32\vcruntime140_1d.dll'. 
+'CCC.exe' (Win32): Loaded 'C:\Windows\System32\ucrtbased.dll'. 
+The thread 0xa004 has exited with code 0 (0x0).
+The thread 0x8eac has exited with code -1073741510 (0xc000013a).
+The thread 0x9e60 has exited with code -1073741510 (0xc000013a).
+The thread 0x2d14 has exited with code -1073741749 (0xc000004b).
+The program '[6740] CCC.exe' has exited with code -1073741510 (0xc000013a).
